54f85522ebaacf184cad8ef32d18c6b10b81d331
fareedmirzay <fareedmirzay@ymail.com>
2024-08-13 15:22:21 -0400

    part 2

--- DOM_Manipulation/index.js
+++ DOM_Manipulation/index.js
@@ -14,4 +14,27 @@ document.addEventListener('DOMContentLoaded', () => {
   // 4. Add a class of flex-ctr to mainEl
   mainEl.classList.add('flex-ctr');
   console.log('Class list of <main> element:', mainEl.classList.value);
+
+
+
+
+
+
+  // Part 2: Creating a Menu Bar for DOM-Manipulation
+  
+  // 1. Select and cache the <nav id="top-menu"> element
+  const topMenuEl = document.querySelector('#top-menu');
+  console.log('Selected <nav id="top-menu"> element:', topMenuEl);
+
+  // 2. Set the height of topMenuEl to 100%
+  topMenuEl.style.height = '100%';
+  console.log('Height of <nav id="top-menu"> set to:', topMenuEl.style.height);
+
+  // 3. Set the background color of topMenuEl
+  topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+  console.log('Background color of <nav id="top-menu"> set to:', getComputedStyle(topMenuEl).backgroundColor);
+
+  // 4. Add a class of flex-around to topMenuEl
+  topMenuEl.classList.add('flex-around');
+  console.log('Class list of <nav id="top-menu">:', topMenuEl.classList.value);
 });
