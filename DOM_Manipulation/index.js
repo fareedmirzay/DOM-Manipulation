04ac00ba92767dc126c39497d55530df87e64a04
fareedmirzay <fareedmirzay@ymail.com>
2024-08-16 10:23:10 -0400

    part 3

--- DOM_Manipulation/index.js
+++ DOM_Manipulation/index.js
@@ -1,3 +1,5 @@
+// Part 1
+
 document.addEventListener('DOMContentLoaded', () => {
   // 1. Select and cache the <main> element
   const mainEl = document.querySelector('main');
@@ -19,9 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-
-  // Part 2: Creating a Menu Bar for DOM-Manipulation
-  
+  // Part 2: Creating a Menu Bar
   // 1. Select and cache the <nav id="top-menu"> element
   const topMenuEl = document.querySelector('#top-menu');
   console.log('Selected <nav id="top-menu"> element:', topMenuEl);
@@ -37,4 +37,34 @@ document.addEventListener('DOMContentLoaded', () => {
   // 4. Add a class of flex-around to topMenuEl
   topMenuEl.classList.add('flex-around');
   console.log('Class list of <nav id="top-menu">:', topMenuEl.classList.value);
+
+
+
+
+
+
+  // Part 3: Adding Menu Buttons
+  const menuLinks = [
+    { href: '#home', text: 'Home' },
+    { href: '#about', text: 'About' },
+    { href: '#services', text: 'Services' },
+    { href: '#contact', text: 'Contact' }
+  ];
+
+  menuLinks.forEach(link => {
+    //Here we are Creating a new <a> element
+    const aEl = document.createElement('a');
+
+    // Here we will set href attribute 
+    aEl.setAttribute('href', link.href);
+
+    // Set the content
+    aEl.textContent = link.text;
+
+    // Append the new <a> element to the <nav id="top-menu"> element
+    topMenuEl.appendChild(aEl);
+
+    // And finally log the creation of the new <a> element
+    console.log('Added <a> element:', aEl);
+  });
 });
